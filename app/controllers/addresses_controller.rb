d0e9c558c2a78a0e4f7e5bd5ac476cdec07ff92f
Stephen Malek <malek.stephen44@gmail.com>
2018-07-10 15:34:29 -0600

    fix stupid misspelling error and finish create new address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -6,7 +6,13 @@ class AddressesController < ApplicationController
 
   def create
     @student = Student.find(params[:student_id])
-    @address = @student.addresses.create(params[:address])
+    @address = @student.addresses.create(address_params)
     redirect_to student_path(@address.student)
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:description, :city, :state, :zip_code, :street, :student_id)
   end
 end
